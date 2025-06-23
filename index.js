e173a38fd4cedca76267fd062160af255eff0216
Your Name <maggiesamwells@gmail.com>
2025-06-23 14:27:20 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const record = [
+  { year: "2015", team: "Denver Broncos", result: "W" },
+  { year: "2014", team: "Seattle Seahawks", result: "L" },
+  { year: "2013", team: "Denver Broncos", result: "L" },
+];
+function superbowlWin(record) {
+  const win = record.find(entry => entry.result === "W");
+  return win ? win.year : undefined;
+}
